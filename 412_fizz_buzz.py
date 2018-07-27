def843358bfad92b62ee4234e07a196bb6c43fad
sanster <cwq1913@gmail.com>
2018-07-27 10:23:28 +0800

    412 fizz buzz

---
+++ 412_fizz_buzz.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        out = []
+        for i in range(1, n + 1):
+            if i % 15 == 0:
+                out.append("FizzBuzz")
+            elif i % 5 == 0:
+                out.append("Buzz")
+            elif i % 3 == 0:
+                out.append("Fizz")
+            else:
+                out.append(str(i))
+
+        return out
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.fizzBuzz(15))
