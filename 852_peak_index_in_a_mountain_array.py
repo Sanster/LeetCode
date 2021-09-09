747e475ed93b878cc0ec842b6c353635199c0d45
Sanster <cwq1913@gmail.com>
2021-09-09 22:40:42 +0800

    852

---
+++ 852_peak_index_in_a_mountain_array.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        # [2, 3, 4, 5, 4] m=2
+        # [1, 2, 5, 4, 3, 2, 1] m=3
+        l = 0
+        r = len(arr) - 1
+        while l < r:
+            m = (l + r) // 2
+            if arr[m] < arr[m + 1]:
+                l = m + 1
+            else:
+                r = m
+        return l
+
+    def peakIndexInMountainArray2(self, arr: List[int]) -> int:
+        # 当前位置是否比 +1 位置大，如果是则返回当前位置
+        for i in range(len(arr) - 1):
+            if arr[i] > arr[i + 1]:
+                return i
+        return len(arr) - 1
+
+
+if __name__ == "__main__":
+    s = Solution()
+    data = [
+        ([0, 1, 0], 1),
+        ([0, 2, 1, 0], 1),
+        ([0, 10, 5, 2], 1),
+        ([3, 4, 5, 1], 2),
+        ([3, 4, 5, 6], 3),
+        ([24, 69, 100, 99, 79, 78, 67, 36, 26, 19], 2),
+    ]
+
+    for it, gt in data:
+        assert s.peakIndexInMountainArray2(it) == gt
+
+    for it, gt in data:
+        assert s.peakIndexInMountainArray(it) == gt
